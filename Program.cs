eb184618beefe4e9a529054fd76fe8ebbe00dd17
Steve Webster <steve.webster190@gmail.com>
2019-01-27 21:27:58 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int count = 1;
+            while (true)
+            {
+                
+                Console.WriteLine("Welcome to the Circle Tester!");
+                Console.Write("Please enter the radius of the circle: ");
+                double userInput = double.Parse(Console.ReadLine());
+
+                Circle myCircle = new Circle(userInput);
+                Console.WriteLine($"Circumference: {myCircle.CalculateCircumference()}");
+                Console.WriteLine($"Circumference Formatted! {myCircle.CalculateFormattedCircumference()}");
+                Console.WriteLine($"Area: {myCircle.CalculateArea()}");
+                Console.WriteLine($"Area Formatted! {myCircle.CalculateFormattedArea()}");
+                
+                Console.WriteLine("Would you like to continue? (y/n)");
+
+                input = Console.ReadLine().ToLower();
+                if (input == "y")
+                {
+                    count++;
+                }
+                else if (input == "n")
+                {
+                    break;
+                }
+
+
+            }
+        }
+    }
+}
+
+
---
+++ Validate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    class Validate
+    {
+        public static bool YesOrNo(string response)
+        {
+
+            while (true)
+            {
+
+                if (response == "y")
+                {
+                    return true;
+                }
+                else if (response == "n")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.Write("Invalid input, try (y/n): ");
+                    response = Console.ReadLine().ToLower();
+
+                }
+            }
+
+        }
+        public static double ValidateRadius()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Please enter the radius of a circle: ");
+                    double num = double.Parse(Console.ReadLine());
+                    return num;
+
+                }
+                catch (FormatException)
+                {
+                    Console.Write("You have a entered an invalid format. Please enter a valid number: ");
+                }
+            }
+        }
+    }
+}
